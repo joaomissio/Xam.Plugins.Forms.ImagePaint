ce1055b0c22b698f4ed0acfc84790c8f86c3ceb8
João Missio <joao-missio@hotmail.com>
2019-02-03 17:37:03 -0200

    Demo Project

---
+++ demo/Demo/Demo.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xam.Plugins.Forms.ImagePaint.Droid;
+
+namespace Demo.Droid
+{
+    [Activity(Label = "Demo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            //Initialize Control
+            ImagePaintRenderer.Init();
+
+            LoadApplication(new App());
+        }
+    }
+}
